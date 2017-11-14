bf3ede933dc292e32ddd266f55f97837a34f81d0
Tommi Viiala <twviiala@gmail.com>
2017-11-14 15:36:33 +0200

    tunteisiiiiiiin

---
+++ koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -0,0 +1,42 @@
+
+package ohtu;
+
+public class Course {
+    private String id;
+    private String name;
+    private String term;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
--- koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -3,6 +3,8 @@ package ohtu;
 import com.google.gson.Gson;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Main {
 
@@ -14,23 +16,39 @@ public class Main {
         }
 
         String url = "https://studies.cs.helsinki.fi/ohtustats/students/" + studentNr + "/submissions";
+        String url2 = "https://studies.cs.helsinki.fi/ohtustats/courseinfo";
+        
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
+        String bodyText2 = Request.Get(url2).execute().returnContent().asString();
+        
+        String statsResponse = "https://studies.cs.helsinki.fi/ohtustats/stats";
+        JsonParser parser = new JsonParser();
+        JsonObject parsittuData = parser.parse(statsResponse).getAsJsonObject();
+        System.out.println("parsittu data");
+//        parsittuData.toString();
 
         System.out.println("json-muotoinen data:");
-//        System.out.println( bodyText );
+        System.out.println( bodyText );
+        System.out.println(bodyText2);
 
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        Gson mapper2 = new Gson();
+        
+        Course course = mapper2.fromJson(bodyText2, Course.class);
+        
+        System.out.println("Kurssi: " + course.getName() + ", " + course.getTerm());
+        System.out.println();
 
         int yhteensa = 0;
         double tunnit = 0;
-        System.out.println("opiskelijanumero: " + studentNr);
+        System.out.println("Opiskelijanumero: " + studentNr);
         System.out.println();
         for (Submission submission : subs) {
             System.out.print("Viikko: ");
             System.out.println(submission.getWeek());
-            System.out.print(", aikaa kului: ");
+            System.out.print("\t, aikaa kului: ");
             System.out.print(submission.getHours());
             tunnit += submission.getHours();
             System.out.print(", tehdyt tehtävät: ");
