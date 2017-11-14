6ce1e9014b8c15bebab7b0773f654a9ca7c41c13
laajaosk <laajaosk@helsinki.fi>
2017-11-14 15:34:07 +0200

    t1

--- koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,41 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "011120775";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/ohtustats/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        int tehtYht = 0;
+        double tuntYht = 0.0;
+        System.out.println("\nOpiskelijanumero: "+studentNr);
+        System.out.println("");
+        for (Submission submission : subs) {
+            System.out.print("Viikko "+submission.getWeek()+": tehtyjä tehtäviä yhteensä: "+submission.getExercises().size()+", aikaa kului "+submission.getHours()+" tuntia, tehdyt tehtävät: ");
+            for (Integer exercise : submission.getExercises()) {
+                System.out.print(exercise+" ");
+            }
+            System.out.println("");
+            tehtYht += submission.getExercises().size();
+            tuntYht += submission.getHours();
+        }
+        
+        System.out.println("");
+        System.out.println("yhteensä: "+tehtYht+" tehtävää "+tuntYht+" tuntia");
+        
     }
 }
--- koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,28 @@
 package ohtu;
 
+import java.util.List;
+
 public class Submission {
     private int week;
+    private double hours;
+    private List<Integer> exercises;
+
+    public List<Integer> getExercises() {
+        return exercises;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setExercises(List<Integer> exercises) {
+        this.exercises = exercises;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+    
 
     public void setWeek(int week) {
         this.week = week;
