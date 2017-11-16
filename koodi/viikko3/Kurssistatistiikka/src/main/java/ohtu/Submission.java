23ffb2f6273f232b736f46c068f4845a7654f34c
Marko Koskinen <markokoskinen2037@gmail.com>
2017-11-16 19:08:26 +0200

    vaihdettu windowsin encoding

--- koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ koodi/viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,19 +1,47 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
+    
     private int week;
+    private int hours;
+    private ArrayList<Integer> exercises;
+    private int max;
+    
+
+    public Submission(int week, int hours, ArrayList exercises) {
+        this.week = week;
+        this.hours = hours;
+        this.exercises = exercises;
+    }
+    
 
     public void setWeek(int week) {
         this.week = week;
     }
 
+    public int getHours() {
+        return hours;
+    }
+
+    public ArrayList<Integer> getExercises() {
+        return exercises;
+    }
+
     public int getWeek() {
         return week;
+    }
+    
+    public void setMax(int max){
+        this.max = max;
     }
 
     @Override
     public String toString() {
-        return ""+week;
+        return "viikko " + this.week + " tehtyjä tehtäviä yhteensä: " + this.exercises.size() + " (maksimi:" + this.max +  "), aikaa kului " + this.hours + " tuntia, tehdyt tehtävät: " + this.exercises;
     }
+
+
     
 }
