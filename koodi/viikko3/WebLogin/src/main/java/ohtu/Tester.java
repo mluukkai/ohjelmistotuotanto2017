e007cc5d9d1d068278d3ba4d1b66c270dd06e8f1
Marko Koskinen <markokoskinen2037@gmail.com>
2017-11-17 17:03:52 +0200

    tehtävä 10 tehty

--- koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,34 +8,85 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Beast\\Desktop\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
-        
-        sleep(2);
-        
+
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
-
-        sleep(2);
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
+
+        element.submit();
         
-        sleep(2);
+        //Toinen testi --------------------------------------------------------------------------------------------------
+
+        driver.get("http://localhost:4567");
+
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akk1111111111ep");
+        element = driver.findElement(By.name("login"));
+
+        element.submit();
+
+
+        //Kolmas testi --------------------------------------------------------------------------------------------------
+        
+        driver.get("http://localhost:4567");
+
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkasdasdssddsssddsdsa");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akk1111111111ep");
+        element = driver.findElement(By.name("login"));
+
         element.submit();
 
         sleep(3);
+       
         
+        //Neljäs testi --------------------------------------------------------------------------------------------------
+        
+        driver.get("http://localhost:4567");
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("uusitili");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("salasana");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("salasana");
+        element = driver.findElement(By.name("signup"));
+
+        element.submit();
+
+        sleep(3);
+
         driver.quit();
+
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
