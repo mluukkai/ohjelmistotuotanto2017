35acc285057238316bce54875a3cc46e6d5197c1
Ouzii <oskari.laaja@gmail.com>
2017-12-05 16:13:17 +0200

    VK6T3

--- koodi/viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ koodi/viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -3,16 +3,13 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
+
+            Matcher m = new HasFewerThan(1, "goals"); 
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
         }
     }
 }
