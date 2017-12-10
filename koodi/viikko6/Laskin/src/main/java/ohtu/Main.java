9afd44a2965ae0eca4c982f523908d3bd1b0c8d4
Marko Koskinen <markokoskinen2037@gmail.com>
2017-12-10 16:50:18 +0200

    poistettu viikko 6

--- koodi/viikko6/Laskin/src/main/java/ohtu/Main.java
+++
@@ -1,9 +0,0 @@
-package ohtu;
-
-import javax.swing.SwingUtilities;
- 
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new GraafinenLaskin());
-    }
-}
